367ae5995161c47f66b0278ea948487112fa004f
Gabriel Rocha <silver250903@gmail.com>
2023-08-07 21:54:08 -0300

    melhorado

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-const emailL = document.getElementById("input1"),
-    senhaL = document.getElementById("input2"),
-    botaoL = document.getElementById("btnLogin"),
-    emailES = document.getElementById("btn2"),
+//Página "cadastro"
+
+const input0 = document.getElementById("input0"),
+    select = document.getElementById("select"),
+    input1 = document.getElementById("input1"),
+    input2 = document.getElementById("input2"),
+    input3 = document.getElementById("input3"),
+    olho = document.getElementById("eye");
+
+
+
+    
+    
+    
+    function mudaOlho() {
+        const cadastro = {
+            nome: input0.value,
+            pais: select.value,
+            data: input1.value,
+            email: input2.value,
+            senha: input3.value
+        }
+        if (olho.checked) {
+            input3.type = "text";
+            console.log(cadastro);
+    } else {
+        input3.type = "password"
+    }
+}
+
+
+// Página "esqueceuSenha"
+
+const emailES = document.getElementById("btn2"),
     redefine = document.getElementById("redefinido"),
-    divs = document.querySelector(".displayN");
+    divs = document.querySelectorAll(".displayN"),
+    emailInput = document.querySelector("#emailCadastro");
 
-const login = {
-    email: emailL,
-    senha: senhaL
-}
-
-if (emailES.value.includes("@") && emailES.value != ""){
-    emailES.addEventListener('click', function(){
+emailES.addEventListener('click', function () {
+    if (emailInput.value.includes("@") && emailInput.value != "") {
+        console.log("oi");
         redefine.style.display = "";
-        divs.setAttribute("hidden");
-    }) ;
-}
-
+        divs.forEach(div => {
+            div.setAttribute("hidden", true);
+        });
+    }
+});
