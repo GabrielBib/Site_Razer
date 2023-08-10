cb95d42a745802260f7d75cf7a34402e514ec477
Gabriel Rocha <silver250903@gmail.com>
2023-08-09 21:53:29 -0300

    atualização

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
 //Página "cadastro"
 
-const input0 = document.getElementById("input0"),
+const input = document.querySelectorAll(".inputs"),
+    input0 = document.getElementById("input0"),
     select = document.getElementById("select"),
     input1 = document.getElementById("input1"),
     input2 = document.getElementById("input2"),
     input3 = document.getElementById("input3"),
-    olho = document.getElementById("eye");
+    olho = document.getElementById("eye"),
+    botao = document.getElementById("btnA"),
+    inputE = document.getElementById("guardaEmail"),
+    inputS = document.getElementById("guardaSenha"),
+    inputObrigatorio = document.getElementById("inputCaptcha");
 
 
 
-    
-    
-    
-    function mudaOlho() {
-        const cadastro = {
-            nome: input0.value,
-            pais: select.value,
-            data: input1.value,
-            email: input2.value,
-            senha: input3.value
-        }
-        if (olho.checked) {
-            input3.type = "text";
-            console.log(cadastro);
+const cadastro = {
+    nome: "",
+    pais: "",
+    data: 0,
+    email: "",
+    senha: ""
+}
+
+function guardaLogin() {
+    if (!inputObrigatorio.checked) {
+        alert("Por favor faça o Captcha");
+
+    } else if (input0.value == "") {
+        alert("Verifique se o campo de NOME foi preenchido corretamente!");
+    } else if (select.value == "País"){
+        alert("Verifique se o campo de País foi escolhido!");
+    } else if (input1.value == "" || ){
+        alert("Verifique se a data é valida!");
+    } else if (input2.value.includes("@") == false || input2.value == ""){
+        alert("Verifique se todos os campos foram preenchidos corretamente! A senha deve ter no mínimo 8 caracteres");
+    } else if (input3.value.length < 8){
+        alert("Verifique se todos os campos foram preenchidos corretamente! A senha deve ter no mínimo 8 caracteres");
     } else {
-        input3.type = "password"
+        cadastro.nome = input0.value;
+        cadastro.pais = select.value;
+        cadastro.data = input1.value;
+        cadastro.email = input2.value;
+        cadastro.senha = input3.value;
+
+        inputE.value += input2.value;
+        inputS.value += input3.value;
+
+        input.value = "";
+
+        // window.location.href = ""
+
+        console.log(inputE.value);
+        console.log(inputS.value);
+
+    }
+}
+
+function mudaOlho() {
+    if (olho.checked) {
+        input3.type = "text";
+    } else {
+        input3.type = "password";
     }
 }
 
